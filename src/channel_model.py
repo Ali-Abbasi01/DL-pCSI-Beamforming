3171e23a3905a0c2f6b2a51ade3e33fca785b86d
Ali Abbasi <ali.abbasi@colorado.edu>
2025-02-05 20:20:28 -0700

    add UIU channel model

--- src/channel_model.py
+++ src/channel_model.py
@@ -58,3 +58,18 @@ class beam_netwrok():
     def generate(self, L = None):
         H = self.calculate_Br() @ self.calculate_A(L) @ (self.calculate_Bt().conj().T)
         return H
+
+
+class UIU():
+
+    def __init__(self, H_bar, U_R, U_T, G):
+        self.H_bar = H_bar
+        self.U_R = U_R
+        self.U_T = U_T
+        self.G = G
+
+    def generate(self):
+        n_R, n_T = H_bar.shape
+        H_tilda = torch.randn(n_R, n_T)
+        H = self.H_bar + self.U_R @ (H_tilda * torch.square(self.G)) @ (self.U_T.conj().T)
+        return H
