20d4bb307f3f4e703d2c54d5aae488503ff7f229
Ali Abbasi <ali.abbasi@colorado.edu>
2025-02-13 22:02:05 -0700

    Add water-filling algorithm

--- src/beamforming.py
+++ src/beamforming.py
@@ -67,4 +67,46 @@ class algorithm():
             if flag == 0: break
         print("The optimal beamforming matrix is:\n", self.V)
         print("\n\n")
-        print("The optimal power allocation is:\n", self.P)                     
+        print("The optimal power allocation is:\n", self.P) 
+
+def water_filling(H, pt):
+  
+  U, S, V = np.linalg.svd(H)
+
+  delta = 1/(S**2)
+
+  S = np.concatenate((S, np.zeros(V.shape[0]-S.shape[0])), axis = 0)
+
+  def f(wl):
+    s = 0
+    for i in delta:
+      s+=max(0, wl-i)
+    return s
+
+  wll = np.arange(min(delta), pt+max(delta)+1, 0.01)
+  p = np.array([f(i) for i in wll])
+
+  #water_level vs. total_power curve
+  wll_levels = 1/wll
+  plt.plot(p, wll_levels)
+  plt.show()
+
+
+  wl_star = wll[np.argmin(np.absolute(p-pt))]
+  print('The water level is:', 1/wl_star)
+
+  diags = np.zeros(S.shape[0])
+  for i in range(S.shape[0]):
+    if S[i]==0:
+      diags[i] = 0
+    else:
+      diags[i] = max(0, wl_star-(1/(S[i]**2)))
+  # diags = [max(0, wl_star-delta[0]), max(0, wl_star-delta[1]),max(0, wl_star-delta[2]),0,0]
+  D = np.diag(diags)
+  Sigma = (V.conj().T)@D@V
+
+  print('The information rate is:')
+  print(D)
+  print(V.conj().T)
+  print("\n\n\n delta is \n", delta)
+  return np.log2(np.linalg.det(np.identity(3)+H@Sigma@(H.conj().T)))                    
