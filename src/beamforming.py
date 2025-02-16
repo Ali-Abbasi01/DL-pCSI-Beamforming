aca3590acc5777e1f4e4d4a9c12344d8457e7160
Ali Abbasi <ali.abbasi@colorado.edu>
2025-02-15 20:45:12 -0700

    water-filling algorithm complete

--- src/beamforming.py
+++ src/beamforming.py
@@ -1,5 +1,5 @@
 #Algorithm
-class algorithm():
+class UIU_algorithm():
 
     def __init__(self, chan_gen, SNR, num_samples=10000):
         self.chan_gen = chan_gen
@@ -69,44 +69,28 @@ class algorithm():
         print("\n\n")
         print("The optimal power allocation is:\n", self.P) 
 
-def water_filling(H, pt):
-  
-  U, S, V = np.linalg.svd(H)
-
+def wf_algorithm(H, pt): 
+  U, S, V = torch.svd(H)
   delta = 1/(S**2)
-
-  S = np.concatenate((S, np.zeros(V.shape[0]-S.shape[0])), axis = 0)
-
+  S = torch.cat((S, torch.zeros(V.shape[0] - S.shape[0], dtype=S.dtype)), dim=0)
   def f(wl):
     s = 0
     for i in delta:
       s+=max(0, wl-i)
     return s
-
-  wll = np.arange(min(delta), pt+max(delta)+1, 0.01)
-  p = np.array([f(i) for i in wll])
-
-  #water_level vs. total_power curve
-  wll_levels = 1/wll
-  plt.plot(p, wll_levels)
-  plt.show()
-
-
-  wl_star = wll[np.argmin(np.absolute(p-pt))]
-  print('The water level is:', 1/wl_star)
-
-  diags = np.zeros(S.shape[0])
+  wll = torch.arange(torch.min(delta), pt+torch.max(delta)+1, 0.01)
+  p = torch.tensor([f(i) for i in wll])
+  wl_star = wll[torch.argmin(torch.abs(p - pt))]
+  diags = torch.zeros(S.shape[0], dtype=torch.complex64)
   for i in range(S.shape[0]):
     if S[i]==0:
       diags[i] = 0
     else:
       diags[i] = max(0, wl_star-(1/(S[i]**2)))
   # diags = [max(0, wl_star-delta[0]), max(0, wl_star-delta[1]),max(0, wl_star-delta[2]),0,0]
-  D = np.diag(diags)
+  D = torch.diag(diags)
   Sigma = (V.conj().T)@D@V
-
-  print('The information rate is:')
-  print(D)
-  print(V.conj().T)
-  print("\n\n\n delta is \n", delta)
-  return np.log2(np.linalg.det(np.identity(3)+H@Sigma@(H.conj().T)))                    
+  print("The optimal beamforming matrix is:\n", V.conj().T)
+  print("The optimal power allocation is:\n", D)
+  return Sigma                   
+              
