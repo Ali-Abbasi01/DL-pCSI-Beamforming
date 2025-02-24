3b9e063dd721a6b0cd1cc87234594b0316e686f1
Ali Abbasi <ali.abbasi@colorado.edu>
2025-02-24 07:49:30 -0700

    Add UIU_cvx and fix fixed_channel_cvx; cvx complete

--- src/cvx.py
+++ src/cvx.py
@@ -2,81 +2,66 @@ import cvxpy as cp
 import numpy as np
 import torch
 
-class fixed_channel():
+class fixed_channel_cvx():
 
-  def __init__(H, PT, solver=cp.SCS):
-    self.H = H
-    self.PT = PT
-    self.soolver = solver
+  def __init__(self, H, PT, solver=cp.SCS):
+      self.H = H.numpy()
+      self.PT = PT
+      self.solver = solver
 
-  def solve():
-    m, n = self.H.shape
-    Sigma = cp.Variable((n, n), hermitian=True)
-    obj = (cp.log_det(np.eye(m) + self.H @ Sigma @ self.H.conj().T)) / np.log(2.0)
-    constraints = [Sigma >> 0, cp.trace(Sigma) <= self.PT]  
-    prob = cp.Problem(cp.Maximize(obj), constraints)
-    if solver == cp.SCS:
-        prob.solve(solver=cp.SCS, max_iters=20000, eps=1e-7, verbose=False)
-    else:
-        prob.solve(solver=solver, verbose=False)
-    return Sigma.value, prob.value, prob.status
-
-
-class UIU_channel():
-
-  def __init__(chan_gen, num_samples=10000):
-    self.chan_gen = chan_gen
-    self.n_R, self.n_T = chan_gen.generate().shape
-    self.num_samples = num_samples
-    self.H_bar = chan_gen.H_bar
-    if torch.all(self.H_bar == 0):
-        self.V = chan_gen.U_T
-    else:
-        U, S, VT = torch.linalg.svd(self.H_bar.T@self.H_bar)
-        self.V = U
-    self.P = torch.eye(self.n_T)
-    self.SNR = SNR
-
-  def solve():
-    
+  def solve(self):
+      m, n = self.H.shape
+      Sigma = cp.Variable((n, n), hermitian=True)
+      obj = (cp.log_det(np.eye(m) + self.H @ Sigma @ self.H.conj().T)) / np.log(2.0)
+      constraints = [Sigma >> 0, cp.trace(Sigma) <= self.PT]  
+      prob = cp.Problem(cp.Maximize(obj), constraints)
+      if self.solver == cp.SCS:
+          prob.solve(solver=cp.SCS, max_iters=20000, eps=1e-7, verbose=False)
+      else:
+          prob.solve(solver=solver, verbose=False)
+      return Sigma.value, prob.value, prob.status
 
 
-def solve_mimo_capacity_cvxpy(H, PT, solver=cp.SCS):
-    """
-    Solve:
-      maximize  log_det(I + H*Sigma*H^H)
-      subject to  Sigma >= 0,  trace(Sigma) <= PT
-    using CVXPY, with a user-chosen solver.
-    """
-    m, n = H.shape
-    
-    # Sigma: n x n, Hermitian
-    Sigma = cp.Variable((n, n), hermitian=True)
-    
-    # log_det(I + H*Sigma*H^H)
-    obj = (cp.log_det(np.eye(m) + H @ Sigma @ H.conj().T)) / np.log(2.0)
-    
-    constraints = [Sigma >> 0, cp.trace(Sigma) <= PT]
-    
-    prob = cp.Problem(cp.Maximize(obj), constraints)
-    
-    # Increase iterations and tighten tolerance if using SCS
-    if solver == cp.SCS:
-        prob.solve(solver=cp.SCS, max_iters=20000, eps=1e-7, verbose=False)
-    else:
-        prob.solve(solver=solver, verbose=False)
+class UIU_cvx():
 
-    return Sigma.value, prob.value, prob.status
+  def __init__(self, chan_gen, num_samples=10000):
+      self.chan_gen = chan_gen
+      self.n_R, self.n_T = chan_gen.generate().shape
+      self.num_samples = num_samples
+      self.H_bar = chan_gen.H_bar
 
-# Test
-if __name__ == "__main__":
-    m, n = 4, 4
-    H = np.random.randn(m, n) + 1j*np.random.randn(m, n)
-    PT = 10.0
+  def solve(self, PT, solver=cp.SCS):
+      H_list = []
+      for _ in range(self.num_samples):
+          H_list.append(self.chan_gen.generate().numpy())
+      H_array = np.array(H_list)
+      K, m, n = H_array.shape
 
-    Sigma_opt_cvx, val_cvx, status = solve_mimo_capacity_cvxpy(H, 10)
-    
-    print("Solver status:", status)
-    print("CVX Optimal objective:", val_cvx)
-    print("Trace of Sigma from CVX:", np.trace(Sigma_opt_cvx))
-    # Compare to water-filling objective or your own calculation
+      # Define the PSD variable Sigma
+      Sigma = cp.Variable((n, n), hermitian=True)
+
+      # Build up sum of log_det(...) over the K realizations
+      sum_logdet = 0
+      I_m = np.eye(m)
+      for k in range(K):
+          Hk = H_array[k]
+          # log_det(I_m + H_k * Sigma * H_k^H)
+          sum_logdet += cp.log_det(I_m + Hk @ Sigma @ Hk.conj().T)
+
+      objective = (1.0 / K) * sum_logdet / np.log(2.0)
+
+      # Constraints: Sigma >= 0 and trace(Sigma) <= P_T
+      constraints = [Sigma >> 0, cp.trace(Sigma) <= PT]
+
+      # Set up problem and solve
+      prob = cp.Problem(cp.Maximize(objective), constraints)
+
+      prob.solve(solver=cp.SCS, max_iters=20000, eps=1e-7, verbose=False)
+
+      if solver == cp.SCS:
+          # Adjust iterations/tolerance for SCS as needed
+          prob.solve(solver=cp.SCS, max_iters=20000, eps=1e-7, verbose=False)
+      else:
+          prob.solve(solver=solver, verbose=False)
+
+      return Sigma.value, prob.value, prob.status
